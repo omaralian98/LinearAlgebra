4999c63862c918f7190785c4bf71171b4f2821a4
omaralian98 <omaralian9870@gmail.com>
2024-03-24 22:08:52 +0300

    Used Theory in Unit tests

--- LinearConsole/Program.cs
+++ LinearConsole/Program.cs
@@ -2,6 +2,7 @@
 using BenchmarkDotNet.Attributes;
 using LinearAlgebra;
 using LinearAlgebra.Classes;
+using MathNet.Numerics.LinearAlgebra.Complex;
 namespace Mr_Sure21
 {
     public class Program
@@ -30,15 +31,15 @@ namespace Mr_Sure21
             ////TimeSpan elapsedTime1 = Stopwatch.GetElapsedTime(startTime1);
             ////Console.WriteLine("Took: {0}", elapsedTime1.TotalSeconds);
             ////Console.WriteLine(result.GetMatrix());
-            Fraction[,] matrix =
-            {
-                { 3, -7, 5, 3, 1, -9 },
-                { 4, 5, 1, 5, 3, 4 }
-            };
+            var matrix = new string[,] { { "10/2", "1", "2" }, { "1", "3", "7" }, { "2", "7", "6" } };
             //var res = Linear.LU<Fraction, float, decimal>(matrix);
             //Console.WriteLine(res.Description);
             //Console.WriteLine((res.U, res.L).GetMatrix());
-            matrix.GetTMatrix<Fraction>();
+            var res = Linear.REFWithResult<Fraction, Fraction, string, int>(matrix, [1, 1, 1]);
+            foreach (var item in res)
+            {
+                (item.Matrix, item.Coefficient).Print();
+            }
             //Console.WriteLine(Linear.Add<Fraction, Fraction>(fractions[0], fractions[1]).GetMatrix());
             //Console.WriteLine(Linear.Add<decimal, Fraction>(fractions[0], fractions[1]).GetMatrix());
             //Console.WriteLine(Linear.Add<double, Fraction>(fractions[0], fractions[1]).GetMatrix());
--- Unit/REFTests.cs
+++ Unit/REFTests.cs
@@ -1,121 +1,177 @@
-namespace Unit
-{
-    public class REFTests
-    {
-        [Fact]
-        public void Test1()
-        {
-            decimal[,] matrix = { { 5, 1, 2 }, { 1, 3, 7 }, { 2, 7, 6 } };
-            decimal[,] expectedValue = { { 5, 1, 2 }, { 0, 14m / 5, 33m / 5 }, { 0, 0, -145m / 14 } };
-            decimal[,] realValue = Linear.REF(matrix);
-            Assert.Equal(expectedValue, realValue);
-        }
+namespace Unit;
 
-        [Fact]
-        public void Test2()
-        {
-            double[,] matrix = { { 5, 6, 6 }, { 1, 9, 5 }, { 4, 5, 9 } };
-            decimal[,] expectedValue = { { 5, 6, 6 }, { 0, 39m / 5, 19m / 5 }, { 0, 0, 160m / 39 } };
-            decimal[,] realValue = Linear.REF<decimal, double>(matrix);
-            Assert.Equal(expectedValue, realValue);
-        }
-        [Fact]
-        public void Test3()
-        {
-            string[,] matrix = { { "10/2", "1", "2" }, { "1", "3", "7" }, { "2", "7", "6" } };
-            decimal[,] expectedValue = { { 5, 1, 2 }, { 0, 14m / 5, 33m / 5 }, { 0, 0, -145m / 14 } };
-            decimal[,] realValue = Linear.REF<decimal, string>(matrix);
-            Assert.Equal(expectedValue, realValue);
-        }
-        [Fact]
-        public void Test4()
-        {
-            string[,] matrix = { { "10/2", "1", "2" }, { "1", "3", "7" }, { "2", "7", "6" } };
-            string[,] expectedValue = { { "5", "1", "2" }, { "0", "14/5", "33/5" }, { "0", "0", "-145/14" } };
-            string[,] realValue = Linear.REF<string, string>(matrix);
-            Assert.Equal(expectedValue, realValue);
-        }
-        [Fact]
-        public void Test6()
-        {
-            string[,] matrix = { { "10/0", "1", "2" }, { "1", "3", "7" }, { "2", "7", "6" } };
-            Assert.Throws<DivideByZeroException>(() => Linear.REF(matrix));
-        }
-        [Fact]
-        public void Test7()
-        {
-            string[,] matrix = { { "1", "1", "2" }, { "1", "3", "7" }, { "2", "6", "6" } };
-            string[] coe = { "0", "39/5" };
-            Assert.Throws<ArgumentException>(() => Linear.REF(matrix, coe));
-        }
-        [Fact]
-        public void Test8()
-        {
-            decimal[,] matrix = { { 1, 3 }, { 9, 3 }, { 1, 9 } };
-            decimal[,] expectedValue = { { 1, 3 }, { 0, -24 }, { 0, 0 } };
-            var realValue = Linear.REF(matrix);
-            Assert.Equal(expectedValue, realValue);
-        }
-        [Fact]
-        public void Test9()
-        {
-            decimal[,] matrix = { { 3, 5 }, { 8, 5 }, { 9, 8 }, { 1, 6 } };
-            string[,] expectedValue = { { "3", "5" }, { "0", "-25/3" }, { "0", "0" }, { "0", "0" } };
-            var realValue = Linear.REF<string, decimal>(matrix);
-            Assert.Equal(expectedValue, realValue);
-        }
-        [Fact]
-        public void Testa1()
-        {
-            decimal[,] matrix = { { 2, 2, 1, 7 }, { 7, 0, 9, 7 } };
-            string[,] expectedValue = { { "2", "2", "1", "7" }, { "0", "-7", "11/2", "-35/2" } };
-            var realValue = Linear.REF<string, decimal>(matrix);
-            Assert.Equal(expectedValue, realValue);
-        }
-        [Fact]
-        public void Testa2()
-        {
-            decimal[,] matrix = { { 1, 4, 1, 5 }, { 1, 4, 0, 5 }, { 2, 8, 7, 6 } };
-            decimal[,] expectedValue = { { 1, 4, 1, 5 }, { 0, 0, -1, 0 }, { 0, 0, 0, -4 } };
-            var realValue = Linear.REF(matrix);
-            Assert.Equal(expectedValue, realValue);
-        }
-        [Fact]
-        public void Testa3()
-        {
-            decimal[,] matrix = { { 1, 1, 2 }, { 1, 3, 7 }, { 2, 6, 6 } };
-            decimal[] coefficient = { 1, 1, 1 };
-            decimal[,] expectedValue = { { 1, 1, 2 }, { 0, 2, 5 }, { 0, 0, -8 } };
-            decimal[] expectedValueCoe = { 1, 0, -1 };
-            var value = Linear.REF(matrix, coefficient);
-            Assert.Equal(expectedValue, value.Matrix);
-            Assert.Equal(expectedValueCoe, value.Coefficient);
-        }
-        [Fact]
-        public void Testa4()
-        {
-            decimal[,] matrix = { { 1, 0, 3, 7, 2 }, { 4, 6, 9, 5, 4 }, { 4, 9, 7, 6, 0 }, { 6, 6, 7, 5, 5 }, { 2, 4, 9, 7, 1 } };
-            decimal[,] expectedValue = { { 1, 0, 3, 7, 2 }, { 0, 6, -3, -23, -4 }, { 0, 0, -1m / 2, 25m / 2, -2 }, { 0, 0, 0, -214, 29 }, { 0, 0, 0, 0, -727m / 321 } };
-            var realValue = Linear.REF(matrix);
-            Assert.Equal(expectedValue, realValue);
-        }
-        [Fact]
-        public void Testa5()
-        {
-            decimal[,] matrix =
-            {
-                { 1, 1, 1, 1, 2 },
-                { -1, -1, -1, 1, 2 },
-                { 3, 3, 3, 4, 7 }
-            };
-            decimal[,] expectedValue =
-            {
-                { 1, 1, 1, 1, 2 },
-                { 0, 0, 0, 2, 4 },
-                { 0, 0, 0, 0, -1 }
-            };
-            var realValue = Linear.REF(matrix);
-            Assert.Equal(expectedValue, realValue);
-        }
+public class REFTests
+{
+    public static IEnumerable<object[]> GetREFMatrixData()
+    {
+        yield return new object[] {
+            new decimal[,] { { 5, 1, 2 }, { 1, 3, 7 }, { 2, 7, 6 } },
+            new decimal[,] { { 5, 1, 2 }, { 0, 14m / 5, 33m / 5 }, { 0, 0, -145m / 14 } }
+        };
+        yield return new object[] {
+            new double[,] { { 5, 6, 6 }, { 1, 9, 5 }, { 4, 5, 9 } },
+            new decimal[,] { { 5, 6, 6 }, { 0, 39m / 5, 19m / 5 }, { 0, 0, 160m / 39 } }
+        };
+        yield return new object[] {
+            new string[,] { { "10/2", "1", "2" }, { "1", "3", "7" }, { "2", "7", "6" } },
+            new decimal[,] { { 5, 1, 2 }, { 0, 14m / 5, 33m / 5 }, { 0, 0, -145m / 14 } }
+        };
+        yield return new object[] {
+            new string[,] { { "10/2", "1", "2" }, { "1", "3", "7" }, { "2", "7", "6" } },
+            new string[,] { { "5", "1", "2" }, { "0", "14/5", "33/5" }, { "0", "0", "-145/14" } }
+        };
+        yield return new object[] {
+             new decimal[,] { { 1, 3 }, { 9, 3 }, { 1, 9 } },
+             new decimal[,] { { 1, 3 }, { 0, -24 }, { 0, 0 } }
+        };
+        yield return new object[] {
+            new decimal[,] { { 3, 5 }, { 8, 5 }, { 9, 8 }, { 1, 6 } },
+            new string[,] { { "3", "5" }, { "0", "-25/3" }, { "0", "0" }, { "0", "0" } }
+        };
+        yield return new object[] {
+            new decimal[,] { { 2, 2, 1, 7 }, { 7, 0, 9, 7 } },
+            new string[,] { { "2", "2", "1", "7" }, { "0", "-7", "11/2", "-35/2" } }
+        };
+        yield return new object[] {
+            new decimal[,] { { 1, 4, 1, 5 }, { 1, 4, 0, 5 }, { 2, 8, 7, 6 } },
+            new decimal[,] { { 1, 4, 1, 5 }, { 0, 0, -1, 0 }, { 0, 0, 0, -4 } }
+        };
+        yield return new object[] {
+            new decimal[,] { { 1, 0, 3, 7, 2 }, { 4, 6, 9, 5, 4 }, { 4, 9, 7, 6, 0 }, { 6, 6, 7, 5, 5 }, { 2, 4, 9, 7, 1 } },
+            new decimal[,] { { 1, 0, 3, 7, 2 }, { 0, 6, -3, -23, -4 }, { 0, 0, -1m / 2, 25m / 2, -2 }, { 0, 0, 0, -214, 29 }, { 0, 0, 0, 0, -727m / 321 } }
+        };
+        yield return new object[] {
+            new decimal[,] { { 1, 1, 1, 1, 2 }, { -1, -1, -1, 1, 2 }, { 3, 3, 3, 4, 7 } }, 
+            new decimal[,] { { 1, 1, 1, 1, 2 }, { 0, 0, 0, 2, 4 }, { 0, 0, 0, 0, -1 } }
+        };
     }
+    public static IEnumerable<object[]> GetREFMatrixAndCoefficientData()
+    {
+        yield return new object[] {
+            new decimal[,] { { 5, 1, 2 }, { 1, 3, 7 }, { 2, 7, 6 } },
+            new int[] { 0, 0, 0 },
+            new decimal[,] { { 5, 1, 2 }, { 0, 14m / 5, 33m / 5 }, { 0, 0, -145m / 14 } },
+            new int[] { 0, 0, 0 }
+        };
+        yield return new object[] {
+            new double[,] { { 5, 6, 6 }, { 1, 9, 5 }, { 4, 5, 9 } },
+            new int[] { 1, 1, 1 },
+            new decimal[,] { { 5, 6, 6 }, { 0, 39m / 5, 19m / 5 }, { 0, 0, 160m / 39 } },
+            new Fraction[] { 1, "4/5", "7/39"}
+        };
+        yield return new object[] {
+            new string[,] { { "10/2", "1", "2" }, { "1", "3", "7" }, { "2", "7", "6" } },
+            new int[] { 1, 1, 1 },
+            new decimal[,] { { 5, 1, 2 }, { 0, 14m / 5, 33m / 5 }, { 0, 0, -145m / 14 } },
+            new Fraction[] { 1, "4/5", "-9/7" }
+        };
+        yield return new object[] {
+            new string[,] { { "10/2", "1", "2" }, { "1", "3", "7" }, { "2", "7", "6" } },
+            new int[] { 1, 1, 1 },
+            new string[,] { { "5", "1", "2" }, { "0", "14/5", "33/5" }, { "0", "0", "-145/14" } },
+            new Fraction[] { 1, "4/5", "-9/7"}
+        };
+        yield return new object[] {
+             new decimal[,] { { 1, 3 }, { 9, 3 }, { 1, 9 } },
+             new int[] { 1, 1, 1 },
+             new decimal[,] { { 1, 3 }, { 0, -24 }, { 0, 0 } },
+             new Fraction[] { 1, -8, -2 }
+        };
+        yield return new object[] {
+            new decimal[,] { { 3, 5 }, { 8, 5 }, { 9, 8 }, { 1, 6 } },
+            new int[] { 1, 1, 1, 1 },
+            new string[,] { { "3", "5" }, { "0", "-25/3" }, { "0", "0" }, { "0", "0" } },
+            new Fraction[] { 1, "-5/3", "-3/5", "-1/5" }
+        };
+        yield return new object[] {
+            new decimal[,] { { 2, 2, 1, 7 }, { 7, 0, 9, 7 } },
+            new int[] { 1, 1 },
+            new string[,] { { "2", "2", "1", "7" }, { "0", "-7", "11/2", "-35/2" } },
+            new Fraction[] { 1, "-5/2" }
+        };
+        yield return new object[] {
+            new decimal[,] { { 1, 4, 1, 5 }, { 1, 4, 0, 5 }, { 2, 8, 7, 6 } },
+            new int[] { 1, 1, 1 },
+            new decimal[,] { { 1, 4, 1, 5 }, { 0, 0, -1, 0 }, { 0, 0, 0, -4 } },
+            new Fraction[] { 1, 0, -1 }
+        };
+        yield return new object[] {
+            new decimal[,] { { 1, 0, 3, 7, 2 }, { 4, 6, 9, 5, 4 }, { 4, 9, 7, 6, 0 }, { 6, 6, 7, 5, 5 }, { 2, 4, 9, 7, 1 } },
+            new int[] { 1, 1, 1, 1, 1 },
+            new decimal[,] { { 1, 0, 3, 7, 2 }, { 0, 6, -3, -23, -4 }, { 0, 0, -1m / 2, 25m / 2, -2 }, { 0, 0, 0, -214, 29 }, { 0, 0, 0, 0, -727m / 321 } },
+            new Fraction[] { 1, -3, "3/2", -26, "-64/321" }
+        };
+        yield return new object[] {
+            new decimal[,] { { 1, 1, 1, 1, 2 }, { -1, -1, -1, 1, 2 }, { 3, 3, 3, 4, 7 } },
+            new int[] { 1, 1, 1 },
+            new decimal[,] { { 1, 1, 1, 1, 2 }, { 0, 0, 0, 2, 4 }, { 0, 0, 0, 0, -1 } },
+            new Fraction[] { 1, 2, -3 }
+        };
+        yield return new object[] {
+            new decimal[,] { { 1, 1, 2 }, { 1, 3, 7 }, { 2, 6, 6 } },
+            new decimal[] { 1, 1, 1 },
+            new decimal[,] { { 1, 1, 2 }, { 0, 2, 5 }, { 0, 0, -8 } },
+            new decimal[] { 1, 0, -1 }
+        };
+    }
+    public static IEnumerable<object[]> GetREFMatrixShouldThrowExceptionData()
+    {
+        yield return new object[] {
+            new string[,] { { "10/0", "1", "2" }, { "1", "3", "7" }, { "2", "7", "6" } },
+            new DivideByZeroException()
+        };
+        yield return new object[] {
+            new string[,] { { "10//", "1", "2" }, { "1", "3", "7" }, { "2", "7", "6" } },
+            new FormatException()
+        };
+        yield return new object[] {
+            new string[,] { { "10/0.1/2", "1", "2" }, { "1", "3", "7" }, { "2", "7", "6" } },
+            new FormatException()
+        };
+    }
+    public static IEnumerable<object[]> GetREFMatrixShouldNotThrowExceptionData()
+    {
+        yield return new object[] {
+
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(GetREFMatrixData))]
+    public void REFOfMatrixTheory<T1, T2>(T1[,] matrix, T2[,] expected)
+    {
+        var realValue = Linear.REF<T2, T1>(matrix);
+        Assert.Equal(expected, realValue);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetREFMatrixAndCoefficientData))]
+    public void REFOfMatrixAndCoefficientTheory<T1, T2, T3, T4>(T1[,] matrix, T2[] coefficient, T3[,] expectedMatrix, T4[] expectedCoefficient)
+    {
+        var value = Linear.REF<T3, T4, T1, T2>(matrix, coefficient);
+        Assert.Equal(expectedMatrix, value.Matrix);
+        Assert.Equal(expectedCoefficient, value.Coefficient);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetREFMatrixShouldThrowExceptionData))]
+    public void REFOfMatrixShouldThrowExceptionTheory<T, E>(T[,] matrix, E type) where E : Exception
+    {
+        Assert.Throws<E>(() => Linear.REF(matrix));
+    }
+
+    [Theory]
+    [MemberData(nameof(GetREFMatrixShouldNotThrowExceptionData))]
+    public void REFOfMatrixShouldNotThrowExceptionTheory<T>(T[,] matrix)
+    {
+        Linear.REF(matrix);
+    }
+
+    //[Fact]
+    //public void Test7()
+    //{
+    //    string[,] matrix = { { "1", "1", "2" }, { "1", "3", "7" }, { "2", "6", "6" } };
+    //    string[] coe = { "0", "39/5" };
+    //    Assert.Throws<ArgumentException>(() => Linear.REF(matrix, coe));
+    //}
 }
