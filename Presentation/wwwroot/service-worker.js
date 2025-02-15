f9d028b5adaac409851d1923f1f33dfb3a20c5b9
Mr_Sure21 <52558137+omaralian98@users.noreply.github.com>
2025-02-15 19:49:57 +0300

    A possible fix for integrity issue

--- Presentation/wwwroot/service-worker.js
+++ Presentation/wwwroot/service-worker.js
@@ -1,4 +1,16 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
+//self.addEventListener('fetch', () => { });
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                '/LinearAlgebra/index.html',
+                '/LinearAlgebra/service-worker.js',
+                // other assets...
+            ]);
+        })
+    );
+});
