ba599fe7aba503c911e5442ea300e301b5a24777
Mr_Sure21 <52558137+omaralian98@users.noreply.github.com>
2025-02-21 19:43:30 +0300

    Testing Notifying users on new updates

--- Presentation/wwwroot/service-worker.js
+++ Presentation/wwwroot/service-worker.js
@@ -2,15 +2,3 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
-
-//self.addEventListener('install', (event) => {
-//    event.waitUntil(
-//        caches.open(CACHE_NAME).then((cache) => {
-//            return cache.addAll([
-//                '/LinearAlgebra/index.html',
-//                '/LinearAlgebra/service-worker.js',
-//                // other assets...
-//            ]);
-//        })
-//    );
-//});
